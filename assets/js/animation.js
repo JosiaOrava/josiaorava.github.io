c88330207f26878583375f0d698b0e6f6a26d838
Josia Orava <josia.orava@gmail.com>
2022-01-30 16:12:12 +0200

    initial commit

---
+++ assets/js/animation.js
@@ -0,0 +1,142 @@
+const anim = document.getElementById("L1");
+anim.onmouseenter = function() {document.getElementById("L1").classList.add("animated")};
+anim.addEventListener("animationend", () => {
+  document.getElementById("L1").classList.remove("animated");
+})
+const anim1 = document.getElementById("L2");
+anim1.onmouseenter = function() {document.getElementById("L2").classList.add("animated")};
+anim1.addEventListener("animationend", () => {
+  document.getElementById("L2").classList.remove("animated");
+})
+const anim2 = document.getElementById("L3");
+anim2.onmouseenter = function() {document.getElementById("L3").classList.add("animated")};
+anim2.addEventListener("animationend", () => {
+  document.getElementById("L3").classList.remove("animated");
+})
+const anim3 = document.getElementById("L4");
+anim3.onmouseenter = function() {document.getElementById("L4").classList.add("animated")};
+anim3.addEventListener("animationend", () => {
+  document.getElementById("L4").classList.remove("animated");
+})
+const anim4 = document.getElementById("L5");
+anim4.onmouseenter = function() {document.getElementById("L5").classList.add("animated")};
+anim4.addEventListener("animationend", () => {
+  document.getElementById("L5").classList.remove("animated");
+})
+const anim5 = document.getElementById("L6");
+anim5.onmouseenter = function() {document.getElementById("L6").classList.add("animated")};
+anim5.addEventListener("animationend", () => {
+  document.getElementById("L6").classList.remove("animated");
+})
+const anim6 = document.getElementById("L7");
+anim6.onmouseenter = function() {document.getElementById("L7").classList.add("animated")};
+anim6.addEventListener("animationend", () => {
+  document.getElementById("L7").classList.remove("animated");
+})
+const anim7 = document.getElementById("L8");
+anim7.onmouseenter = function() {document.getElementById("L8").classList.add("animated")};
+anim7.addEventListener("animationend", () => {
+  document.getElementById("L8").classList.remove("animated");
+})
+const anim8 = document.getElementById("L9");
+anim8.onmouseenter = function() {document.getElementById("L9").classList.add("animated")};
+anim8.addEventListener("animationend", () => {
+  document.getElementById("L9").classList.remove("animated");
+})
+const anim9 = document.getElementById("L10");
+anim9.onmouseenter = function() {document.getElementById("L10").classList.add("animated")};
+anim9.addEventListener("animationend", () => {
+  document.getElementById("L10").classList.remove("animated");
+})
+const anim10 = document.getElementById("L11");
+anim10.onmouseenter = function() {document.getElementById("L11").classList.add("animated")};
+anim10.addEventListener("animationend", () => {
+  document.getElementById("L11").classList.remove("animated");
+})
+const anim11 = document.getElementById("L12");
+anim11.onmouseenter = function() {document.getElementById("L12").classList.add("animated")};
+anim11.addEventListener("animationend", () => {
+  document.getElementById("L12").classList.remove("animated");
+})
+const anim12 = document.getElementById("L13");
+anim12.onmouseenter = function() {document.getElementById("L13").classList.add("animated")};
+anim12.addEventListener("animationend", () => {
+  document.getElementById("L13").classList.remove("animated");
+})
+const anim13 = document.getElementById("L14");
+anim13.onmouseenter = function() {document.getElementById("L14").classList.add("animated")};
+anim13.addEventListener("animationend", () => {
+  document.getElementById("L14").classList.remove("animated");
+})
+const anim14 = document.getElementById("L15");
+anim14.onmouseenter = function() {document.getElementById("L15").classList.add("animated")};
+anim14.addEventListener("animationend", () => {
+  document.getElementById("L15").classList.remove("animated");
+})
+const anim15 = document.getElementById("L16");
+anim15.onmouseenter = function() {document.getElementById("L16").classList.add("animated")};
+anim15.addEventListener("animationend", () => {
+  document.getElementById("L16").classList.remove("animated");
+})
+const anim16 = document.getElementById("L17");
+anim16.onmouseenter = function() {document.getElementById("L17").classList.add("animated")};
+anim16.addEventListener("animationend", () => {
+  document.getElementById("L17").classList.remove("animated");
+})
+const anim17 = document.getElementById("L18");
+anim17.onmouseenter = function() {document.getElementById("L18").classList.add("animated")};
+anim17.addEventListener("animationend", () => {
+  document.getElementById("L18").classList.remove("animated");
+})
+const anim18 = document.getElementById("L19");
+anim18.onmouseenter = function() {document.getElementById("L19").classList.add("animated")};
+anim18.addEventListener("animationend", () => {
+  document.getElementById("L19").classList.remove("animated");
+})
+const anim19 = document.getElementById("L20");
+anim19.onmouseenter = function() {document.getElementById("L20").classList.add("animated")};
+anim19.addEventListener("animationend", () => {
+  document.getElementById("L20").classList.remove("animated");
+})
+const anim20 = document.getElementById("L21");
+anim20.onmouseenter = function() {document.getElementById("L21").classList.add("animated")};
+anim20.addEventListener("animationend", () => {
+  document.getElementById("L21").classList.remove("animated");
+})
+const anim21 = document.getElementById("L22");
+anim21.onmouseenter = function() {document.getElementById("L22").classList.add("animated")};
+anim21.addEventListener("animationend", () => {
+  document.getElementById("L22").classList.remove("animated");
+})
+const anim22 = document.getElementById("L23");
+anim22.onmouseenter = function() {document.getElementById("L23").classList.add("animated")};
+anim22.addEventListener("animationend", () => {
+  document.getElementById("L23").classList.remove("animated");
+})
+const anim23 = document.getElementById("L24");
+anim23.onmouseenter = function() {document.getElementById("L24").classList.add("animated")};
+anim23.addEventListener("animationend", () => {
+  document.getElementById("L24").classList.remove("animated");
+})
+const anim24 = document.getElementById("L25");
+anim24.onmouseenter = function() {document.getElementById("L25").classList.add("animated")};
+anim24.addEventListener("animationend", () => {
+  document.getElementById("L25").classList.remove("animated");
+})
+const anim25 = document.getElementById("L26");
+anim25.onmouseenter = function() {document.getElementById("L26").classList.add("animated")};
+anim25.addEventListener("animationend", () => {
+  document.getElementById("L26").classList.remove("animated");
+})
+const anim26 = document.getElementById("L27");
+anim26.onmouseenter = function() {document.getElementById("L27").classList.add("animated")};
+anim26.addEventListener("animationend", () => {
+  document.getElementById("L27").classList.remove("animated");
+})
+const anim27 = document.getElementById("L28");
+anim27.onmouseenter = function() {document.getElementById("L28").classList.add("animated")};
+anim27.addEventListener("animationend", () => {
+  document.getElementById("L28").classList.remove("animated");
+})
+
+
