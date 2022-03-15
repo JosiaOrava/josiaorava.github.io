0ac8af4925be32f32168f4b5d107c854792eeed0
Josia Orava <josia.orava@gmail.com>
2022-03-15 22:21:34 +0200

    Sorting algorithm

---
+++ assets/sortingvisualizer/sortingVisualizer.js
@@ -0,0 +1,93 @@
+var generateBtn = document.getElementById("newArrayBtn");
+var sortBtn = document.getElementById("sortBtn");
+const sortingArray = [];
+generateBtn.onclick = function(){
+    newArray();
+}
+sortBtn.onclick = function(){
+    var sortedArray = quickSort(sortingArray, 0, sortingArray.length -1);
+    removeElementsByClass("arrChildNew");
+    for(i in sortedArray){
+        var y = document.createElement("div");
+        y.className = "arrChildNew"
+        var calcHeight = sortingArray[i] * 5 + "px";
+        y.style.height = calcHeight;
+        y.innerHTML = sortingArray[i];
+        document.getElementById("arrChild").appendChild(y);
+    }
+}
+function newArray(){
+    
+    if(document.getElementById("arrChild").hasChildNodes()){    
+        removeElementsByClass("arrChildNew");
+    }
+    
+    for(i=0; i<115; i++){
+        sortingArray.push(getRandomInt(5, 100));
+        console.log(sortingArray[i]);
+    }
+    //const map = sortingArray.map(x => x);
+    //console.log(map);
+    for(i in sortingArray){
+        var y = document.createElement("div");
+        y.className = "arrChildNew"
+        var calcHeight = sortingArray[i] * 5 + "px";
+        y.style.height = calcHeight;
+        y.innerHTML = sortingArray[i];
+        document.getElementById("arrChild").appendChild(y);
+    }
+    
+}
+function removeElementsByClass(className){
+    
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function swap(items, leftIndex, rightIndex){
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
+}
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
+    }
+    return items;
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Added Math.floor to round numbers to int
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
